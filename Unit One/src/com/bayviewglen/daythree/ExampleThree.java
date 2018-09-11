b6a99b8a6a0cceeb4ee8415954ddd8f9b5381fa6
Ryu202021 <43181933+Ryu202021@users.noreply.github.com>
2018-09-11 13:41:01 -0400

    init commit

---
+++ Unit One/src/com/bayviewglen/daythree/ExampleThree.java
@@ -0,0 +1,25 @@
+package com.bayviewglen.daythree;
+
+public class ExampleThree {
+	/* Types of Errors */
+	public static void main(String[] args) {
+		//ints x, y;	// Syntax error - Your Java is WRONG! Java does not understand you
+					// and will not run if you have syntax errors
+		
+		// Run-time errors - errors that occur at RUN TIME.
+		//int x = 7;
+		
+		//int y = x / 0;
+		
+		System.out.println(y);
+		
+		// Logic Errors
+		int radius = 7;
+		double areaCircle = Math.PI * 2 * radius;	// logic mistake because the equation is wrong
+													// these are more like glitches
+													// difficult to find and generally get missed
+		System.out.println(areaCircle);
+		
+	}
+		
+}
