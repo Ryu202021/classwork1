d53d4037471adfbf39ad83fc1934afa0be701f17
Ryu202021 <43181933+Ryu202021@users.noreply.github.com>
2018-09-12 17:58:31 -0400

    WORK IN PROGRESS

--- Unit One/src/com/bayviewglen/daythree/BayviewGlenPools.java
+++ Unit One/src/com/bayviewglen/daythree/BayviewGlenPools.java
@@ -12,7 +12,9 @@ public class BayviewGlenPools {
 		final double TRANSITION_LENGTH = (Math.sqrt(Math.pow(TRANSITION_HYP, 2)-Math.pow(DE_END_H - SH_END_H, 2)));
 		final double SH_END_L = POOL_LENGTH - ((Math.sqrt((Math.pow(TRANSITION_HYP, 2)) - (Math.pow(DE_END_H, 2)))) + DE_END_L);
 		
-	 
+		//Questions 1 - 5 ^
+		//Other Questions 1 - 3 v
+		
 		double shallowEndBase = SH_END_H * POOL_WIDTH;
 		double shallowEndSides = SH_END_L * SH_END_H;
 		double shallowEndWall = SH_END_L * POOL_WIDTH;
@@ -24,21 +26,34 @@ public class BayviewGlenPools {
 		double deepEndSA = deepEndWall + deepEndBase + deepEndSides;
 		
 		double transitionBase = (TRANSITION_HYP * POOL_LENGTH);
-		double transitionSides = (TRANSITION_HYP * DE_END_H) + ((Math.sqrt((Math.pow(TRANSITION_HYP, 2)) - (Math.pow(DE_END_H, 2)))) * SH_END_H) / 2;
+		double transitionSides = ;
 		double transitionSA = transitionBase + (transitionSides * 2);
 		
 		double totalSA = shallowEndSA + deepEndSA + transitionSA;
 		double linerCost = totalSA * 60;
 		double linerTotalCost = Math.round(linerCost * 100) / 100;
 		double linerCents = (linerCost * 100 % 100);
+	
 		
-		System.out.println("1. The pool length is " + POOL_LENGTH + " .");
-		System.out.println("2. The depth of the shallow end is " + SH_END_H + " and the depth of the deep end is "
-				+ DE_END_H + " .");
+		double a = DE_END_H - SH_END_H;
+		double b = TRANSITION_LENGTH;
+		double c = TRANSITION_HYP;
+		double h = POOL_WIDTH;
+		double volumeOfPool = POOL_LENGTH * POOL_WIDTH * DE_END_H;
+		
+		double minusTriPrism = 1 / 4 * h (Math.sqrt(-1 * a + 2 * (Math.pow(a * b, 2)) + 2 * (Math.pow(a * c, 2)) - (Math.pow(b, 4)) + 2 * (Math.pow(b * c, 2)) - Math.pow(c, 4)));
+		
+		System.out.println("1. The pool length is " + POOL_LENGTH + "m.");
+		System.out.println("2. The depth of the shallow end is " + SH_END_H + "m and the depth of the deep end is "
+				+ DE_END_H + "m.");
 		System.out.println("3. The length of transition between the shallow end and the deep end is " + TRANSITION_LENGTH + " .");
-		System.out.println("4. The length of the shallow end is " + SH_END_L + " .");
-		System.out.println("5. The price of the liner is $60/m^2. The price for the pool is $" + (int)linerTotalCost + "." + (int)linerCents + ".");
-
+		System.out.println("4. The length of the shallow end is " + SH_END_L + "m.");
+		System.out.println("5. The price of the liner is $60/m^2. ");
+		
+		System.out.println("1. The amount of water needed to fill the pool 90% is " + " of water.");
+		System.out.println("2. The amount of liner needed is " + totalSA + "m^2.");
+		System.out.println("3. The price for the entire pool's liner $" + (int)linerTotalCost + "." + (int)linerCents + ".");
+		
 	}
 
 }
